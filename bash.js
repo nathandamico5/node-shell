57ad88d2aabd5257ecbc1c2a7bd4955da3bea373
Morgan Mack <mackleemorgan@gmail.com>
2021-03-05 14:35:09 -0500

    The Process Global

--- bash.js
+++ bash.js
@@ -1,1 +1,10 @@
-test;
+process.stdout.write('prompt > ');
+
+process.stdin.on('data', (data) => {
+  const cmd = data.toString().trim();
+
+  let retVal = process.cwd();
+
+  process.stdout.write(retVal);
+  process.stdout.write('\nprompt > ');
+});
